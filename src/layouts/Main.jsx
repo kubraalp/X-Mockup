15f23db99780bdb738f940722eeec43769940c91
Kubra Alp <kubra.alp.k@gmail.com>
2024-06-25 09:19:31 +0300

    pages created

---
+++ src/layouts/Main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+import SideBar from "./SideBar"
+
+const Container = styled.div`
+  width: 1265px;
+  margin: auto;
+  display: flex;
+`;
+export default function Main() {
+  return <Container>
+    <SideBar/>
+Selam Arkadaşlar
+  </Container>;
+}
---
+++ src/layouts/SideBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 275px;
+  background-color: red;
+  min-height: 100vh;
+`;
+
+export default function SideBar() {
+  return <Container>SideBarrrrrrrrrrrrrrrr</Container>;
+}
